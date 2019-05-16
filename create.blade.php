77e7738018d09c466cec4844d4e9cc8ae6ea9876
masami tawara <masamix.c0m@hotmail.co.jp>
2019-05-16 00:16:37 +0000

    laravel.13.課題4

--- create.blade.php
+++ create.blade.php
@@ -1,13 +1,67 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MyNews</title>
-    </head>
-    <body>
-        <h1>Myプロフィール作成画面</h1>
-    </body>
-</html>
+　　　　{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+
+{{-- profile.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'Myプロフィール')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール</h2>
+                 <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">タイトル</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="body">本文</label>
+                        <div class="col-md-10">
+                            
+                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}
+                            1．氏名(name)：タワラ　マサミ
+                            
+                            2．性別(gender)：女
+                            
+                            3．趣味(hobby)：料理
+                            
+                            4．自己紹介欄(introduction)：
+                            　　　プログラミング言語を学ぶ一児の母です
+                        　
+                            
+                            </textarea>
+                           
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">画像</label>
+                        <div class="col-md-10">
+                            
+                            <input type="file" class="form-control-file" name="image">
+                        </div>
+                    </div>
+                    
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">   
+                        <input type="text" name=""/>
+                        <input type="submit" value="Submit"/>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
+　　　　
+　　　
